1c25961cc3a4e3f37ed97f4ad94040e5ba5bfca5
Ilia Burakov <burakov.ilia@gmail.com>
2025-02-25 05:47:35 -0500

    add unit tests for Domain layer

---
+++ DiplomaticMailBot.Tests.Unit/DomainServices/PollOptionParserTests.cs
@@ -0,0 +1,127 @@
+using DiplomaticMailBot.Common.Enums;
+using DiplomaticMailBot.Common.Extensions;
+using DiplomaticMailBot.Domain;
+using LanguageExt.Common;
+
+namespace DiplomaticMailBot.Tests.Unit.DomainServices;
+
+[TestFixture]
+public sealed class PollOptionParserTests
+{
+    private PollOptionParser _parser;
+
+    [SetUp]
+    public void Setup()
+    {
+        _parser = new PollOptionParser();
+    }
+
+    [Test]
+    public void GetMessageId_WithValidInput_ReturnsMessageId()
+    {
+        // Arrange
+        var pollOptionText = "[123] Test message";
+
+        // Act
+        var result = _parser.GetMessageId(pollOptionText);
+
+        // Assert
+        Assert.That(result.Match(err => err.Code, success => success), Is.EqualTo(123));
+    }
+
+    [Test]
+    public void GetMessageId_WithEmptyInput_ReturnsError()
+    {
+        // Arrange
+        var pollOptionText = string.Empty;
+
+        // Act
+        var result = _parser.GetMessageId(pollOptionText);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.Match(err => err.Code, success => success), Is.EqualTo(EventCode.MessageIdNotFound.ToInt()));
+        });
+    }
+
+    [Test]
+    public void GetMessageId_WithoutOpeningBracket_ReturnsError()
+    {
+        // Arrange
+        var pollOptionText = "123] Test message";
+
+        // Act
+        var result = _parser.GetMessageId(pollOptionText);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.Match(err => err.Code, success => success), Is.EqualTo(EventCode.OpeningBracketNotFound.ToInt()));
+        });
+    }
+
+    [Test]
+    public void GetMessageId_WithoutClosingBracket_ReturnsError()
+    {
+        // Arrange
+        var pollOptionText = "[123 Test message";
+
+        // Act
+        var result = _parser.GetMessageId(pollOptionText);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.Match(err => err.Code, success => success), Is.EqualTo(EventCode.ClosingBracketNotFound.ToInt()));
+        });
+    }
+
+    [Test]
+    public void GetMessageId_WithEmptyBrackets_ReturnsError()
+    {
+        // Arrange
+        var pollOptionText = "[] Test message";
+
+        // Act
+        var result = _parser.GetMessageId(pollOptionText);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.Match(err => err.Code, success => success), Is.EqualTo(EventCode.MessageIdNotFound.ToInt()));
+        });
+    }
+
+    [Test]
+    public void GetMessageId_WithSpaceInBrackets_ReturnsError()
+    {
+        // Arrange
+        var pollOptionText = "[ ] Test message";
+
+        // Act
+        var result = _parser.GetMessageId(pollOptionText);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.Match(err => err.Code, success => success), Is.EqualTo(EventCode.MessageIdNotFound.ToInt()));
+        });
+    }
+
+    [Test]
+    public void GetMessageId_WithNonNumericContent_ReturnsError()
+    {
+        // Arrange
+        var pollOptionText = "[abc] Test message";
+
+        // Act
+        var result = _parser.GetMessageId(pollOptionText);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.Match(err => err.Code, success => success), Is.EqualTo(EventCode.MessageIdNotFound.ToInt()));
+        });
+    }
+}
--- DiplomaticMailBot.Tests.Unit/DomainServices/PreviewGeneratorTests.cs
+++ DiplomaticMailBot.Tests.Unit/DomainServices/PreviewGeneratorTests.cs
@@ -33,7 +33,7 @@ public sealed class PreviewGeneratorTests
         // Arrange
         var message = new Message
         {
-            Photo = [new PhotoSize()],
+            Photo = new[] { new PhotoSize() },
             Caption = "Beautiful sunset",
         };
 
@@ -50,7 +50,7 @@ public sealed class PreviewGeneratorTests
         // Arrange
         var message = new Message
         {
-            Photo = [new PhotoSize()],
+            Photo = new[] { new PhotoSize() },
         };
 
         // Act
@@ -127,6 +127,190 @@ public sealed class PreviewGeneratorTests
 
         // Assert
         Assert.That(result, Is.EqualTo("general (General Chat)"));
+    }
+
+    [Test]
+    public void GetMessagePreview_WithAnimation_ReturnsAnimationPreview()
+    {
+        // Arrange
+        var message = new Message { Animation = new Animation(), Caption = "Funny GIF" };
+
+        // Act
+        var result = _previewGenerator.GetMessagePreview(message, 50);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("(Анимация) Funny GIF"));
+    }
+
+    [Test]
+    public void GetMessagePreview_WithAudio_ReturnsAudioPreview()
+    {
+        // Arrange
+        var message = new Message { Audio = new Audio(), Caption = "Nice song" };
+
+        // Act
+        var result = _previewGenerator.GetMessagePreview(message, 50);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("(Аудио) Nice song"));
+    }
+
+    [Test]
+    public void GetMessagePreview_WithDocument_ReturnsDocumentPreview()
+    {
+        // Arrange
+        var message = new Message { Document = new Document(), Caption = "Important file" };
+
+        // Act
+        var result = _previewGenerator.GetMessagePreview(message, 50);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("(Документ) Important file"));
+    }
+
+    [Test]
+    public void GetMessagePreview_WithSticker_ReturnsStickerPreview()
+    {
+        // Arrange
+        var message = new Message { Sticker = new Sticker() };
+
+        // Act
+        var result = _previewGenerator.GetMessagePreview(message, 50);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("Стикер"));
+    }
+
+    [Test]
+    public void GetMessagePreview_WithVideo_ReturnsVideoPreview()
+    {
+        // Arrange
+        var message = new Message { Video = new Video(), Caption = "Cool video" };
+
+        // Act
+        var result = _previewGenerator.GetMessagePreview(message, 50);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("(Видео) Cool video"));
+    }
+
+    [Test]
+    public void GetMessagePreview_WithVideoNote_ReturnsVideoNotePreview()
+    {
+        // Arrange
+        var message = new Message { VideoNote = new VideoNote() };
+
+        // Act
+        var result = _previewGenerator.GetMessagePreview(message, 50);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("Видеосообщение"));
+    }
+
+    [Test]
+    public void GetMessagePreview_WithVoice_ReturnsVoicePreview()
+    {
+        // Arrange
+        var message = new Message { Voice = new Voice() };
+
+        // Act
+        var result = _previewGenerator.GetMessagePreview(message, 50);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("Голосовое сообщение"));
+    }
+
+    [Test]
+    public void GetMessagePreview_WithLocation_ReturnsLocationPreview()
+    {
+        // Arrange
+        var message = new Message { Location = new Location() };
+
+        // Act
+        var result = _previewGenerator.GetMessagePreview(message, 50);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("Местоположение"));
+    }
+
+    [Test]
+    public void GetPollOptionPreview_WithValidInput_ReturnsFormattedString()
+    {
+        // Act
+        var result = _previewGenerator.GetPollOptionPreview(123, "user", "test message", 10, 50);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("[123] (user): test message"));
+    }
+
+    [Test]
+    public void GetPollOptionPreview_WithLongAuthorName_TruncatesAuthorName()
+    {
+        // Act
+        var result = _previewGenerator.GetPollOptionPreview(123, "verylongusername", "test message", 5, 50);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("[123] (veryl): test message"));
+    }
+
+    [Test]
+    public void GetPollOptionPreview_WithTotalLengthLimit_TruncatesEntireString()
+    {
+        // Act
+        var result = _previewGenerator.GetPollOptionPreview(123, "user", "very long test message that should be truncated", 10, 20);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("[123] (user): very l"));
+    }
+
+    [Test]
+    public void GetMessageLinkUrl_WithValidInput_ReturnsFormattedUrl()
+    {
+        // Act
+        var result = _previewGenerator.GetMessageLinkUrl(-1001234567890, 123);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("https://t.me/c/1234567890/123"));
+    }
+
+    [Test]
+    public void GetMessageLinkMarkdown_WithMessageIdOnly_ReturnsFormattedMarkdown()
+    {
+        // Act
+        var result = _previewGenerator.GetMessageLinkMarkdown(-1001234567890, 123);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("[123](https://t.me/c/1234567890/123)"));
+    }
+
+    [Test]
+    public void GetMessageLinkMarkdown_WithCustomCaption_ReturnsFormattedMarkdown()
+    {
+        // Act
+        var result = _previewGenerator.GetMessageLinkMarkdown(-1001234567890, 123, "Click here");
+
+        // Assert
+        Assert.That(result, Is.EqualTo("[Click here](https://t.me/c/1234567890/123)"));
+    }
+
+    [Test]
+    public void GetMessageLinkHtml_WithMessageIdOnly_ReturnsFormattedHtml()
+    {
+        // Act
+        var result = _previewGenerator.GetMessageLinkHtml(-1001234567890, 123);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("""<a href="https://t.me/c/1234567890/123">123</a>"""));
+    }
+
+    [Test]
+    public void GetMessageLinkHtml_WithCustomCaption_ReturnsFormattedHtml()
+    {
+        // Act
+        var result = _previewGenerator.GetMessageLinkHtml(-1001234567890, 123, "Click here");
+
+        // Assert
+        Assert.That(result, Is.EqualTo("""<a href="https://t.me/c/1234567890/123">Click here</a>"""));
     }
 
     private static Message CreateMessage(
